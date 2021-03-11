4b27619afeecf51f445af8f929751d6da182b5a0
Flávio Renato <peudevbr@gmail.com>
2021-03-10 22:16:32 -0300

    React Concepts Challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,45 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(newTaskTitle ==='') {
+      return;
+    }
+
+    const date = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, date])
+    setNewTaskTitle("")
+
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task)
+      
+    setTasks(editedTask)
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    tasks.map(task => {
+      if (task.id === id) {
+        const index = tasks.indexOf(task)
+        const array = tasks
+        array.splice(index, 1)
+        setTasks([...array])
+
+      }      
+    })
   }
 
   return (
